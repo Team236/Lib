e2b31a1085249f93bdc05aee51c1e908d2aaaa4e
Sam Fuchs <samcfuchs@gmail.com>
2018-02-19 00:45:05 -0500

    Add WaitCommand
    Common interface to start a command on a delay

---
+++ commands/WaitCommand.java
@@ -0,0 +1,31 @@
+package lib.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class WaitCommand extends CommandGroup {
+
+	/**
+	 * Start a command on a delay
+	 * @param delay The delay before the command starts, in seconds
+	 * @param command The command to start
+	 * @param timeout The timeout for the command
+	 */
+	public WaitCommand(double delay, Command command, double timeout) {
+		addSequential(new Wait(delay));
+		addSequential(command, timeout);
+	}
+
+	/**
+	 * Start a command on a delay
+	 * @param delay The delay before the command starts, in seconds
+	 * @param command The command to start
+	 */
+	public WaitCommand(double delay, Command command) {
+		addSequential(new Wait(delay));
+		addSequential(command);
+	}
+}
