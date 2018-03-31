c9a19637db35565db6e84c2640ac3c20c5816d1e
Sam Fuchs <samcfuchs@gmail.com>
2018-03-31 13:52:43 -0500

    Add left and right kV to drive params

--- motionProfile/DriveParameters.java
+++ motionProfile/DriveParameters.java
@@ -1,11 +1,17 @@
 package lib.motionProfile;
 
 public class DriveParameters {
-	public double kP, kV, kA;
+	public double kP, kV, kV_l, kV_r, kA;
 
-	public DriveParameters(double _kV, double _kA, double _kP) {
-		this.kV = _kV;
+	public DriveParameters(double _kV_l, double _kV_r, double _kA, double _kP) {
+		this.kV_l = _kV_l;
+		this.kV_r = _kV_r;
 		this.kA = _kA;
 		this.kP = _kP;
+	}
+	
+	public DriveParameters(double _kV, double _kA, double _kP) {
+		this(_kV, _kV, _kA, _kP);
+		this.kV = _kV;
 	}
 }
